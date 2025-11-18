1a66fce346d2f1ce81e1cb66b0f36575eeee8722
debenji68 <deiber.benji@gmail.com>
2025-11-18 15:59:25 +0100

    exo 8

--- TP2/tp3exo8.py
+++ TP2/tp3exo8.py
@@ -1,4 +1,4 @@
-x = int(input("Entrez un nombre décimal : "))
+x = float(input("Entrez un nombre décimal : "))
 if (2<= x <3) or (0< x <=1) or (-10<= x <=2):
     print(f"{x} appartient à I")
 else:
